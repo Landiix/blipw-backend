de0dc997e5d93f96f94a786b988e739c72eccce5
Landix <mexikanec0077@gmail.com>
2026-07-17 01:00:14 +0300

    feat: добавлена регистрация пользователей с хешированием паролей

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,7 +1,12 @@
 package models
 
+import (
+	"time"
+)
+
 type User struct {
-	Id       int
-	Username string
-	Password string
+	Id        int64     `json:"id"`
+	Username  string    `json:"username"`
+	Password  string    `json:"-"`
+	CreatedAt time.Time `json:"created_at"`
 }
